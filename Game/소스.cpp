e60b9e82f63541f181f3ce4998e8e2b81490c9ae
KIM <anjyounl24@gmail.com>
2023-08-23 19:47:35 +0900

    2023.08.23 게임다이노2

--- Game/소스.cpp
+++ Game/소스.cpp
@@ -9,9 +9,15 @@
 #include <conio.h>
 #include <windows.h>
 
+
+#define UP 72
+#define DOWN 80
+
 #define DINO_BOTTOM_Y 12
 #define TREE_BOTTOM_Y 20
 #define TREE_BOTTOM_X 45
+#define BIRD_BOTTOM_X 35
+#define BIRD_BOTTOM_Y 15
 
 
 
@@ -36,7 +42,105 @@ void gotoXY(int x, int y)
 
 }
 
+//徹左球税 脊径聖 閤壱, 脊径吉 徹税 葵聖 鋼発馬澗 敗呪
+int KeyBoard()
+{
+	if (_kbhit() != 0)
+	{
+		return _getch();
 
+	}
+	return 0;
+}
+//因傑聖 益軒澗 敗呪
+void DrawDino(int dinoY)
+{
+	gotoXY(0, dinoY);
+	static bool legFlag = true;
+	printf("         」」」」」」  \n");
+	printf("       」」 」」」」」」 \n");
+	printf("       」」」」」」」」」 \n");
+	printf("」      」」」       \n");
+	printf("」」     」」」」」」」   \n");
+	printf("」」」   」」」」」      \n");
+	printf(" 」」  」」」」」」」」」」 \n");
+	printf(" 」」」」」」」」」」」    \n");
+	printf("  」」」」」」」」」」     \n");
+	printf("    」」」」」」」」     \n");
+	printf("     」」」」」」      \n");
+
+	if (legFlag)
+	{
+		printf("   」     」」」\n");
+		printf("   」」         ");
+		legFlag = false;
+	}
+	else
+	{
+		printf(" 」」」    」    \n");
+		printf("        」」     ");
+		legFlag = true;
+	}
+}
+void DrawDino(int dinoY)
+{
+	gotoXY(0, dinoY);
+	static bool legFlag = true;
+	
+	printf("       」                                  \n");
+	printf("       」」                              \n");
+	printf("       」」」                        \n");
+	printf("       」」」              」」」」」」」          \n");
+	printf("        」」」」」」」」」」」」」  」」 」」」」」」              \n");
+	printf("        」」」」」」」」」」」」」」 」」」」」」」」」          \n");
+	printf("         」」」」」」」」」」    」」」        \n");
+	printf("          」」」」」」」」     」」」」」」」            \n");
+	printf("                 」                              \n");
+	printf("                 」                               \n");
+	if (legFlag)				   
+	{
+		printf("   」     」」」\n");
+		printf("   」」         ");
+		legFlag = false;
+	}
+	else
+	{
+		printf(" 」」」    」    \n");
+		printf("        」」     ");
+		legFlag = true;
+	}
+}
+//蟹巷研 益軒澗 敗呪
+void Tree(int treeX)
+{
+	gotoXY(treeX, TREE_BOTTOM_Y);
+	printf("」」」」」 ");
+	gotoXY(treeX, TREE_BOTTOM_Y+1);
+	printf(" 」」」 ");
+	gotoXY(treeX, TREE_BOTTOM_Y+2);
+	printf(" 」」」 ");
+	gotoXY(treeX, TREE_BOTTOM_Y+3);
+	printf(" 」」」 ");
+	gotoXY(treeX, TREE_BOTTOM_Y+4);
+	printf(" 」」」 ");
+}
+void GameOver(const int score)
+{
+	system("cls");
+	int x = 18;
+	int y = 8;
+	gotoXY(x, y);
+	printf("===========================");
+	gotoXY(x, y + 1);
+	printf("======G A M E O V E R======");
+	gotoXY(x, y + 2);
+	printf("===========================");
+	gotoXY(x, y + 5);
+	printf("SCORE : %d,", score);
+
+	printf("\n\n\n\n\n\n\n\n\n");
+	system("pause");
+}
 
 
 int main()
